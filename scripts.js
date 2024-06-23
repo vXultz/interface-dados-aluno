439a90543f77ae30f121c09ba5eeb989f6a7c9d7
Vitor Schultz <vitorserta@gmail.com>
2024-06-23 14:44:22 -0300

    usando as infos do localstorage

--- scripts.js
+++ scripts.js
@@ -20,3 +20,21 @@ document.addEventListener('DOMContentLoaded', () => {
   mediaMaterias.push(mediaInicial);
   atualizarMediaGeral();
 });
+
+
+//[M2S06] Ex. 04 - LabScore (Pt.3)
+document.addEventListener('DOMContentLoaded', () => {
+  const dadosAluno = localStorage.getItem('dadosAluno');
+
+  if (dadosAluno) {
+    const aluno = JSON.parse(dadosAluno);
+
+    document.getElementById('nome-aluno').textContent = `Nome: ${aluno.nome}`;
+    document.getElementById('idade-aluno').textContent = `Idade: ${aluno.idade} anos`;
+    document.getElementById('serie-aluno').textContent = `Série: ${aluno.serie}`;
+    document.getElementById('escola-aluno').textContent = `Escola: ${aluno.escola}`;
+    document.getElementById('materia-preferida').textContent = `Matéria preferida: ${aluno.materiaFavorita}`;
+  } else {
+    console.error('Dados do aluno não encontrados no localStorage');
+  }
+});
